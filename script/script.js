eb790e42bbb2759f3af39f15450269af09fc2a97
StarkovAleksey2020 <starkovaleksey2017@gmail.com>
2021-01-29 15:33:24 +0800

    Added code to the complicated task of lesson 02

---
+++ script/script.js
@@ -0,0 +1,10 @@
+let num = 266219;
+let numArray = num.toString().split('');
+let result = 1;
+
+for (let index = 0; index < numArray.length; index++) {
+  result *= Number(numArray[index]);
+}
+console.log('Multiplication result: ', result);
+console.log('The result of raising to the power of 3', result ** 3);
+console.log('First two characters: ', (result ** 3).toString().substr(0,2));
