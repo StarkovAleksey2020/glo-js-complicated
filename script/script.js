8406d29df0d1a9efc11495c41ac4b12cae43365e
StarkovAleksey2020 <starkovaleksey2017@gmail.com>
2021-01-30 08:45:36 +0800

    added: processing an array using the reduce method

--- script/script.js
+++ script/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let num = 266219;
 let numArray = num.toString().split('');
 let result = 1;
@@ -8,3 +10,6 @@ for (let index = 0; index < numArray.length; index++) {
 console.log('Multiplication result: ', result);
 console.log('The result of raising to the power of 3', result ** 3);
 console.log('First two characters: ', (result ** 3).toString().substr(0,2));
+
+const reducer = (accumulator, currentValue) => accumulator * currentValue;
+console.log('Multiplication result with reduce: ', numArray.reduce(reducer));
